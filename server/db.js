4260aa080713d281624e157213a67976872378ee
Gordon <godronus@yahoo.com>
2017-11-03 17:07:38 -0700

    latest for create func

--- server/db.js
+++ server/db.js
@@ -1,4 +1,5 @@
 const gremlinOrm = require('gremlin-orm');
-const g = new gremlinOrm(process.env.GPORT, process.env.GHOST, {ssl: true, user: process.env.GUSER, password: process.env.GPASS}, 'azure');
+// const g = new gremlinOrm('azure', process.env.GPORT, process.env.GHOST, {ssl: true, user: process.env.GUSER, password: process.env.GPASS});
+const g = new gremlinOrm('neo4j');
 
 module.exports = g;
